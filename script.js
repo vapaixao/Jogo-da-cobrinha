2e01d3616abaf3d6e9af5376e5934feca3dfe633
Vagner Paixao <vagnner.ti@gmail.com>
2021-06-08 13:07:20 -0300

    Finalizando o jogo

--- script.js
+++ script.js
@@ -44,6 +44,13 @@ function iniciarJogo(){
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
+    for(i = 1; i < snake.length; i++){
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
     criarBG();
     criarCobrinha();
     drawFood();
@@ -55,8 +62,16 @@ function iniciarJogo(){
     if (direction == "left") snakeX -= box;
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
+
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+        food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
     
-    snake.pop();
+ 
 
     let newHead = {
         x: snakeX,
